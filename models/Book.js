0472fb7493e26aae068b4eb7152451c2524ec6a1
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-04-10 14:41:39 +0200

    working mongoose models

---
+++ models/Book.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+// créons un schéma de données qui contient les champs souhaités pour chaque Book
+const bookSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: Number,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true
+  },
+  ratings: [{
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+    //   ref: 'User',
+      required: true
+    },
+    grade: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5
+    }
+  }],
+  averageRating: {
+    type: Number,
+  }
+});
+
+//exportons ce schéma en tant que modèle Mongoose appelé « Book »
+module.exports = mongoose.model('Book', bookSchema);
