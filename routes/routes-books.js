b5d78504fec720e5c228aeba3260ee8166b56d64
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-04-10 16:31:59 +0200

    cleaning up

--- routes/routes-books.js
+++ routes/routes-books.js
@@ -4,13 +4,15 @@ const multer = require("../middleware/multer-config")
 const router = express.Router()
 const booksController = require("../controllers/controller-books")
 
-
-router.post("/", auth, multer, booksController.createBook)
-router.post("/:id/rating", auth, booksController.addRatingToBook)
 router.get("/", booksController.getAllBooks)
 router.get("/bestrating", booksController.getBestBooks)
 router.get("/:id", booksController.getOneBook)
+
+router.post("/", auth, multer, booksController.createBook)
+router.post("/:id/rating", auth, booksController.addRatingToBook)
+
 router.delete("/:id", auth, booksController.deleteBook)
+
 router.put("/:id", auth, multer, booksController.updateBook)
 
 module.exports = router
