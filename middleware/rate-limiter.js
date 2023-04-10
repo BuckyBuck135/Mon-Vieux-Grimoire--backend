0f129eb36eef472f019a0fd59569bd56ce0532ce
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-04-10 18:18:37 +0200

    rate limit to 20 per minute

--- middleware/rate-limiter.js
+++ middleware/rate-limiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require("express-rate-limit")
 module.exports = rateLimit({
-  windowMs: 1 * 60 * 1000, // fenêtres de 1 minute
-  max: 5, // limite chaque IP à 5 requêtes par 
+  windowMs: 1 * 60 * 1000, // fenêtres de X minute
+  max: 20, // limite chaque IP à Y requêtes par fenêtre
   handler: function (req, res, next) {
     return res.status(429).json({error})
   }
