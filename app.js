bdf40010feade90e75279ee645de90341e63201d
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-04-10 18:11:31 +0200

    cleaning up

--- app.js
+++ app.js
@@ -25,16 +25,6 @@ app.use(cors())
 //     next()
 // })
 
-// const rateLimit = require("express-rate-limit")
-// const apiRequestLimiter = rateLimit({
-//   windowMs: 1 * 60 * 1000, // fenêtres de 1 minute
-//   max: 5, // limite chaque IP à 5 requêtes par 
-//   handler: function (req, res, next) {
-//     return res.status(429).json({error})
-//   }
-// })
-
-// Use the limit rule as an application middleware
 app.use(rateLimit)
 
 app.use("/api/books", booksRoutes)
