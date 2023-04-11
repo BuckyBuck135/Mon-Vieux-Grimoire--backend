d7b50cbf95c6ecece74900a4242c0f1d1ca1ebba
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-04-11 14:26:55 +0200

    fixed error handler

--- middleware/rate-limiter.js
+++ middleware/rate-limiter.js
@@ -1,8 +1,9 @@
 const rateLimit = require("express-rate-limit")
 module.exports = rateLimit({
   windowMs: 1 * 60 * 1000, // fenêtres de X minute
-  max: 20, // limite chaque IP à Y requêtes par fenêtre
   handler: function (req, res, next) {
-    return res.status(429).json({error})
-  }
+    return res.status(429).json({
+      error: "Trop de requêtes. Veuillez réessayer plus tard."
+    })
+  } 
 })
