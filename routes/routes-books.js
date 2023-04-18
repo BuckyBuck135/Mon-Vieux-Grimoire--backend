3a5112bb6c5d4d28aaa8ca04484d1c403f3361c0
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-04-18 21:42:33 +0200

    implemented sharp for image compression

--- routes/routes-books.js
+++ routes/routes-books.js
@@ -1,6 +1,9 @@
 const express = require("express")
+const sharp = require('sharp');
+const fs = require('fs');
 const auth = require("../middleware/auth")
-const multer = require("../middleware/multer-config")
+const multerMW = require("../middleware/multer-config")
+const sharpMW = require("../middleware/sharp-config")
 const router = express.Router()
 const booksController = require("../controllers/controller-books")
 
@@ -8,11 +11,11 @@ router.get("/", booksController.getAllBooks)
 router.get("/bestrating", booksController.getBestBooks)
 router.get("/:id", booksController.getOneBook)
 
-router.post("/", auth, multer, booksController.createBook)
+router.post("/", auth, multerMW, sharpMW, booksController.createBook)
 router.post("/:id/rating", auth, booksController.addRatingToBook)
 
 router.delete("/:id", auth, booksController.deleteBook)
 
-router.put("/:id", auth, multer, booksController.updateBook)
+router.put("/:id", auth, multerMW, booksController.updateBook)
 
 module.exports = router
